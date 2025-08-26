2b1f70e666191b58906ca763ef3478ee0ceeeea7
ThuongHoai081 <th316942@gmail.com>
2025-08-26 17:13:56 +0700

    feat(basic-exercise01): add basic exercise01

---
+++ src/main/java/basic/Exercise01.java
@@ -0,0 +1,27 @@
+package basic;
+
+/**
+ * Exercise01: Find the Greatest Common Divisor (GCD)
+ * and Least Common Multiple (LCM) of two natural numbers.
+ */
+public class Exercise01 {
+
+    // Tim UCLN (Greatest Common Divisor)
+    public int findGcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Tim BCNN (Least Common Multiple)
+    public int findLcm(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return (a * b) / findGcd(a, b);
+    }
+
+}
