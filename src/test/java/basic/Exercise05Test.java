b7c96bacbb938d2ca71fbc5fbf8613fb158a4fc1
ThuongHoai081 <th316942@gmail.com>
2025-08-27 10:04:38 +0700

    fix(basic-exercise05): Add testcase in Exercise05

--- src/test/java/basic/Exercise05Test.java
+++ src/test/java/basic/Exercise05Test.java
@@ -16,5 +16,19 @@ public class Exercise05Test {
         assertEquals(BigInteger.valueOf(2), exercise05.findFactorial(2));
         assertEquals(BigInteger.valueOf(6), exercise05.findFactorial(3));
         assertEquals(BigInteger.valueOf(24), exercise05.findFactorial(4));
+
+        assertEquals(BigInteger.valueOf(720), exercise05.findFactorial(6));
+        assertEquals(BigInteger.valueOf(5040), exercise05.findFactorial(7));
+        assertEquals(BigInteger.valueOf(40320), exercise05.findFactorial(8));
+        assertEquals(BigInteger.valueOf(362880), exercise05.findFactorial(9));
+        assertEquals(new BigInteger("3628800"), exercise05.findFactorial(10));
+        assertEquals(new BigInteger("39916800"), exercise05.findFactorial(11));
+        assertEquals(new BigInteger("479001600"), exercise05.findFactorial(12));
+
+        assertEquals(new BigInteger("6227020800"), exercise05.findFactorial(13));
+        assertEquals(new BigInteger("87178291200"), exercise05.findFactorial(14));
+        assertEquals(new BigInteger("1307674368000"), exercise05.findFactorial(15));
+
+        assertEquals(BigInteger.valueOf(-1), exercise05.findFactorial(-5));
     }
 }
