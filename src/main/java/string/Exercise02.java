f85cc9c69a914bfb1635f837f1e08131037b81d2
ThuongHoai081 <th316942@gmail.com>
2025-08-28 00:05:01 +0700

    feat(string-exercise02): add string exercise02

---
+++ src/main/java/string/Exercise02.java
@@ -0,0 +1,34 @@
+package string;
+
+/**
+ * Exercise02:Calculate the sum of integers in the string.
+ * For example abc 123 def 33 mn 3.221 will give 380 where 380 = 123+33+3+221.
+ */
+public class Exercise02 {
+    public long sumOfIntegersInString(String str) {
+
+        if (str == null || str.isEmpty()) {
+            return 0L;
+        }
+
+        long sum = 0;
+        StringBuilder numStr = new StringBuilder();
+
+        for (char ch : str.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                numStr.append(ch);
+            }
+
+            if (!Character.isDigit(ch) && !numStr.isEmpty()) {
+                sum += Long.parseLong(numStr.toString());
+                numStr.setLength(0);
+            }
+        }
+
+        if (!numStr.isEmpty()) {
+            sum += Long.parseLong(numStr.toString());
+        }
+
+        return sum;
+    }
+}
