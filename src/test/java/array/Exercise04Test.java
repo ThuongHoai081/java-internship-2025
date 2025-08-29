f7e0916fce9066f3a4728399bc135064efdf94e4
ThuongHoai081 <th316942@gmail.com>
2025-08-29 14:26:13 +0700

    feat(array-exercise04): add array exercise04

---
+++ src/test/java/array/Exercise04Test.java
@@ -0,0 +1,19 @@
+package array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class Exercise04Test {
+    @Test
+    void shouldMergeTwoSortedArrays_OK() {
+        final Exercise04 ex = new Exercise04();
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, ex.mergeArrayAndSort(new int[]{1, 3, 5}, new int[]{2, 4, 6}));
+        assertArrayEquals(new int[]{1, 2, 3}, ex.mergeArrayAndSort(new int[]{1, 2, 3}, new int[]{}));
+        assertArrayEquals(new int[]{1, 2, 3}, ex.mergeArrayAndSort(new int[]{}, new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{}, ex.mergeArrayAndSort(new int[]{}, new int[]{}));
+        assertArrayEquals(new int[]{1, 2, 3}, ex.mergeArrayAndSort(null, new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{1, 2, 3}, ex.mergeArrayAndSort(new int[]{1, 2, 3}, null));
+        assertArrayEquals(null, ex.mergeArrayAndSort(null, null));
+    }
+}
